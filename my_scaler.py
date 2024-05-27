0f4848bf5f3248ff9ace413b85d1950dfc34f16f
REUBEN SOLOMON <55707963+Kachanta@users.noreply.github.com>
2024-05-27 23:26:15 +0100

    starting afresh

--- my_scaler.py
+++
@@ -1,9 +0,0 @@
-def data_scaler(x):
-    
-    #Importing the standard scaler
-    from sklearn.preprocessing import StandardScaler
-    scaler = StandardScaler()
-    
-    #Scalling 
-    new_data_scaled = scaler.fit_transform(x)
-    return new_data_scaled
